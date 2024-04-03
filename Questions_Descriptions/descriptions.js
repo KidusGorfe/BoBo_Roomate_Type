e4108f0c9c3c1292bf04934068184208307f86ed
Kidus Gorfe <kidus.gorfe@gmail.com>
2024-04-03 15:46:03 -0500

    Update 2

--- Questions_Descriptions/descriptions.js
+++ Questions_Descriptions/descriptions.js
@@ -1,22 +1,22 @@
 var descriptions = {
     "The Party Rocker": {
-        positive: "Life with a Party Rocker is never dull. Their magnetic personality pulls everyone into their orbit of fun and unpredictability. They're the architects of unforgettable nights and the creators of cherished memories, turning the mundane into the extraordinary.",
-        negative: "Their whirlwind lifestyle means plans change at the blink of an eye, and their concept of 'tidy' might just be 'able to see the floor.' Living with them requires a high tolerance for last-minute changes and a knack for navigating through piles of who-knows-what.",
+        Pros: "Life with a Party Rocker is never dull. Their magnetic personality pulls everyone into their orbit of fun and unpredictability. They're the architects of unforgettable nights and the creators of cherished memories, turning the mundane into the extraordinary.",
+        Cons: "Their whirlwind lifestyle means plans change at the blink of an eye, and their concept of 'tidy' might just be 'able to see the floor.' Living with them requires a high tolerance for last-minute changes and a knack for navigating through piles of who-knows-what.",
         gifUrl: `<iframe src="https://giphy.com/embed/IP9FNJuz0uNLG" width="480" height="241" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/vevo-animated-gif-shufflebots-IP9FNJuz0uNLG">via GIPHY</a></p>` // Direct embed link as a string
     },
     "The Role Model": {
-        positive: "The Role Model is your go-to for a perfectly orchestrated game night or potluck. They combine their love for people with a love for lists, making sure every social gathering runs smoothly. Their organizational skills ensure that bills are paid on time, and the common areas are Pinterest-worthy.",
-        negative: "Sometimes, their love for order can tip into control territory. Their detailed plans don't always leave room for spontaneity, and their color-coded cleaning schedule might feel a bit like homework. And there is a small chance that if things don’t go to plan they might lose it!",
+        Pros: "The Role Model is your go-to for a perfectly orchestrated game night or potluck. They combine their love for people with a love for lists, making sure every social gathering runs smoothly. Their organizational skills ensure that bills are paid on time, and the common areas are Pinterest-worthy.",
+        Cons: "Sometimes, their love for order can tip into control territory. Their detailed plans don't always leave room for spontaneity, and their color-coded cleaning schedule might feel a bit like homework. And there is a small chance that if things don’t go to plan they might lose it!",
         gifUrl: `<iframe src="https://giphy.com/embed/L3ut1TkDIqduYKM21D" width="480" height="357" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/startrek-picard-captain-stng1-L3ut1TkDIqduYKM21D">via GIPHY</a></p>` // Direct embed link as a string
     },
     "The Starving Artist": {
-        positive: "A Starving Artist’s room is an inspiring den of chaos where creativity knows no bounds. Their passionate pursuits can range from painting to coding, often leaving a trail of materials and gadgets in their wake. Their independence is admirable, fueling innovation and self-discovery.",
-        negative: "Their creative process can sometimes mean the rest of the house becomes collateral damage in their latest project. Finding a common space that hasn't been commandeered for their latest masterpiece can be a challenge.",
+        Pros: "A Starving Artist’s room is an inspiring den of chaos where creativity knows no bounds. Their passionate pursuits can range from painting to coding, often leaving a trail of materials and gadgets in their wake. Their independence is admirable, fueling innovation and self-discovery.",
+        Cons: "Their creative process can sometimes mean the rest of the house becomes collateral damage in their latest project. Finding a common space that hasn't been commandeered for their latest masterpiece can be a challenge.",
         gifUrl: `<iframe src="https://giphy.com/embed/xT39D7GQo1m3LatZyU" width="480" height="258" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/endlesspoetry-alejandro-jodorowsky-endless-poetry-xT39D7GQo1m3LatZyU">via GIPHY</a></p>` // Direct embed link as a string
     },
     "The Zen Zone": {
-        positive: "The Zen Zone creates a sanctuary of calm and order. Their minimalist approach and impeccable organization make their space a soothing retreat from the world's chaos. They're reflective, deeply thoughtful, and their self-sufficiency can be a bastion of peace in a hectic household.",
-        negative: "Their need for order and solitude can sometimes clash with the communal aspects of shared living. Their strict adherence to routines and minimalist living might not always accommodate the unpredictable nature of other roommates' lives.",
+        Pros: "The Zen Zone creates a sanctuary of calm and order. Their minimalist approach and impeccable organization make their space a soothing retreat from the world's chaos. They're reflective, deeply thoughtful, and their self-sufficiency can be a bastion of peace in a hectic household.",
+        Cons: "Their need for order and solitude can sometimes clash with the communal aspects of shared living. Their strict adherence to routines and minimalist living might not always accommodate the unpredictable nature of other roommates' lives.",
         gifUrl: `<iframe src="https://giphy.com/embed/3o7btSlEkVbvtjkW3K" width="480" height="257" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/foxhomeent-yoga-peace-3o7btSlEkVbvtjkW3K">via GIPHY</a></p>` // Direct embed link as a string
     }
 };
