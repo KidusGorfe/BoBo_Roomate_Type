da28e9137b1c8c54ce7166ebc4c067436841e5a8
Kidus Gorfe <kidus.gorfe@gmail.com>
2024-04-03 15:44:09 -0500

    Update

--- Questions_Descriptions/questions.js
+++ Questions_Descriptions/questions.js
@@ -2,91 +2,91 @@ var questions = [
   {
     text: "A friend unexpectedly drops by your place. Your first move is:",
     answers: [
-      { text: "Crank up the music and turn it into an impromptu party", type: "The Party Rocker" },
-      { text: "Pull out your planner to see how you can fit this visit into your day", type: "The Role Model" },
-      { text: "Hope they don't mind the creative mess you've currently got going on", type: "The Starving Artist" },
-      { text: "Offer them some homemade herbal tea and a calm place to chat", type: "The Zen Zone" }
+      { text: "Music and party 🎉", type: "The Party Rocker" },
+      { text: "Check planner 📅", type: "The Role Model" },
+      { text: "Creative mess 🎨", type: "The Starving Artist" },
+      { text: "Herbal tea ☕", type: "The Zen Zone" }
     ]
   },
   {
     text: "When you're feeling overwhelmed, you prefer to:",
     answers: [
-      { text: "Hit the town with friends to blow off steam", type: "The Party Rocker" },
-      { text: "Organize your space and to-do list to regain control", type: "The Role Model" },
-      { text: "Escape into your art or a personal project to refocus", type: "The Starving Artist" },
-      { text: "Practice mindfulness or meditation to find your center", type: "The Zen Zone" }
+      { text: "Hit the town 🚀", type: "The Party Rocker" },
+      { text: "Organize space 📝", type: "The Role Model" },
+      { text: "Escape into art 🎨", type: "The Starving Artist" },
+      { text: "Practice mindfulness 🧘", type: "The Zen Zone" }
     ]
   },
   {
     text: "Your approach to household chores is:",
     answers: [
-      { text: "What are chores? Let's have fun instead!", type: "The Party Rocker" },
-      { text: "Creating a shared schedule to ensure everything gets done", type: "The Role Model" },
-      { text: "Chores are secondary to my art, but I'll get to them... eventually", type: "The Starving Artist" },
-      { text: "Maintaining a personal standard of cleanliness and order, hoping others follow", type: "The Zen Zone" }
+      { text: "Have fun 😄", type: "The Party Rocker" },
+      { text: "Shared schedule 📅", type: "The Role Model" },
+      { text: "Chores secondary 🎨", type: "The Starving Artist" },
+      { text: "Maintain cleanliness 🧹", type: "The Zen Zone" }
     ]
   },
   {
     text: "Your idea of a perfect evening involves:",
     answers: [
-      { text: "A lively gathering or night out with a big group of friends", type: "The Party Rocker" },
-      { text: "A well-planned dinner and movie night with close ones", type: "The Role Model" },
-      { text: "Working on your latest creative project in solitude", type: "The Starving Artist" },
-      { text: "A quiet night in, with everything in its right place, and maybe a book", type: "The Zen Zone" }
+      { text: "Lively gathering 🎉", type: "The Party Rocker" },
+      { text: "Well-planned night 🍽️", type: "The Role Model" },
+      { text: "Creative project 🎨", type: "The Starving Artist" },
+      { text: "Quiet night 📚", type: "The Zen Zone" }
     ]
   },
   {
     text: "In a group project, you're the one who:",
     answers: [
-      { text: "Sparks enthusiasm and keeps the energy up", type: "The Party Rocker" },
-      { text: "Takes the lead on planning and delegating tasks", type: "The Role Model" },
-      { text: "Brings creative ideas and an unconventional approach", type: "The Starving Artist" },
-      { text: "Ensures the group's work environment is harmonious and focused", type: "The Zen Zone" }
+      { text: "Keep energy up 💥", type: "The Party Rocker" },
+      { text: "Lead planning 📊", type: "The Role Model" },
+      { text: "Bring creative ideas 🎨", type: "The Starving Artist" },
+      { text: "Ensure harmony ☯️", type: "The Zen Zone" }
     ]
   },
   {
     text: "When it comes to decorating your space, you prioritize:",
     answers: [
-      { text: "Fun, quirky elements that spark conversation", type: "The Party Rocker" },
-      { text: "Aesthetic and functional design that keeps everything in order", type: "The Role Model" },
-      { text: "An eclectic mix that reflects your artistic spirit", type: "The Starving Artist" },
-      { text: "Minimalist designs that create a sense of peace and order", type: "The Zen Zone" }
+      { text: "Spark conversation 🗣️", type: "The Party Rocker" },
+      { text: "Functional design 🛋️", type: "The Role Model" },
+      { text: "Reflect artistic spirit 🎨", type: "The Starving Artist" },
+      { text: "Create peace ☮️", type: "The Zen Zone" }
     ]
   },
   {
     text: "How do you handle conflict with roommates?",
     answers: [
-      { text: "Try to lighten the mood and suggest a compromise over drinks", type: "The Party Rocker" },
-      { text: "Organize a meeting to discuss and resolve the issue logically", type: "The Role Model" },
-      { text: "Prefer to avoid conflict, but will express your feelings through art", type: "The Starving Artist" },
-      { text: "Seek to understand their perspective and find a balanced solution", type: "The Zen Zone" }
+      { text: "Lighten mood 🍹", type: "The Party Rocker" },
+      { text: "Organize meeting 💬", type: "The Role Model" },
+      { text: "Express through art 🎨", type: "The Starving Artist" },
+      { text: "Seek balanced solution ☯️", type: "The Zen Zone" }
     ]
   },
   {
     text: "Planning your weekend looks like:",
     answers: [
-      { text: "Making no plans and letting the adventure find you", type: "The Party Rocker" },
-      { text: "Outlining a list of activities and errands to get through", type: "The Role Model" },
-      { text: "Dedicating time to your passions, away from the world's distractions", type: "The Starving Artist" },
-      { text: "A balance of leisure activities and personal productivity within a serene environment", type: "The Zen Zone" }
+      { text: "Let adventure 🌍", type: "The Party Rocker" },
+      { text: "Outline activities 📋", type: "The Role Model" },
+      { text: "Dedicate to passions 🎨", type: "The Starving Artist" },
+      { text: "Balance leisure 🌿", type: "The Zen Zone" }
     ]
   },
   {
     text: "You're most likely to get annoyed by a roommate who:",
     answers: [
-      { text: "Prefers a quiet house all the time, stifling your fun", type: "The Party Rocker" },
-      { text: "Is unpredictable and messes with your carefully laid plans", type: "The Role Model" },
-      { text: "Tries to impose too much structure on your creative chaos", type: "The Starving Artist" },
-      { text: "Brings too much noise and disruption into your peaceful space", type: "The Zen Zone" }
+      { text: "Stifle your fun 🤫", type: "The Party Rocker" },
+      { text: "Messes with plans 🔄", type: "The Role Model" },
+      { text: "Imposes structure 🎨", type: "The Starving Artist" },
+      { text: "Brings disruption 🚧", type: "The Zen Zone" }
     ]
   },
   {
     text: "When choosing a movie to watch, you prefer something that:",
     answers: [
-      { text: "Keeps everyone laughing and engaged", type: "The Party Rocker" },
-      { text: "Has great reviews and likely appeals to a wide audience", type: "The Role Model" },
-      { text: "Inspires you creatively or challenges your perspective", type: "The Starving Artist" },
-      { text: "Calms your mind and offers a sense of tranquility", type: "The Zen Zone" }
+      { text: "Keep everyone laughing 😆", type: "The Party Rocker" },
+      { text: "Great reviews 🎥", type: "The Role Model" },
+      { text: "Inspires creatively 🎨", type: "The Starving Artist" },
+      { text: "Calms mind 🧘", type: "The Zen Zone" }
     ]
   },
-]
+];
